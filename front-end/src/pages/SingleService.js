77e284d1a7fdf90712edba046998b54ef4408952
ali hassan <aleehassan1999@gmail.com>
2022-09-13 18:41:05 +0500

    single product page complete

--- front-end/src/pages/SingleService.js
+++ front-end/src/pages/SingleService.js
@@ -40,10 +40,96 @@ function SingleService() {
                 </div>
 
                 <div className="descContainer">
-                    <h1>Title</h1>
+
+                    <div className = "prodInfo" >
+
+                        <h1>Facebook Services</h1>
+
+                        <div>We provide reliable facebook follower Services</div>
+
+                        <div className="filterContainer"> 
+
+                            <h2>Select Your Services</h2>
+
+                            <div className="filter">
+
+                            <div>
+
+                                <input type="radio" value="follower" name="follower" /> Followers
+
+                                </div>
+
+                                <select name = "followers" className='select' >
+                                    <option disabled >
+                                        
+                                    </option>
+                                    
+                                    <option>500</option>
+                                    <option>750</option>
+                                    <option>1000</option>
+                                    <option>2000</option>
+                                    <option>5000</option>
+
+
+                                </select>
+                            </div>
+
+                            <div className="filter">
+
+                                  <div>
+                                    <input type="radio" value="like" name="like" /> Likes
+                                    </div>
+
+                                <select name = "likes" className='select' >
+                                    <option disabled >
+                                        
+                                    </option>
+                                    <option> 100 </option>
+                                    <option>250</option>
+                                    <option>500</option>
+                                    <option>750</option>
+                                    <option>1000</option>
+                                    <option>2000</option>
+                                    <option>5000</option>
+                                </select>
+                            </div>
+
+ 
+                            <div className="filter">
+
+                                <div>
+
+                                <input type="radio" value="comment" name="comment" /> Comments
+
+                                </div>
+                                
+
+                                <select name = "comments" className='select' >
+                                    <option disabled >
+                                        
+                                    </option>
+                                    <option> 100 </option>
+                                    <option>250</option>
+                                    <option>500</option>
+                                    <option>750</option>
+                                    <option>1000</option>
+                                    
+                                </select>
+                            </div>
+
+
+
+                        </div>
+
+                        <div className = "buttonContainer">
+
+                            <button className="button"> ADD TO CART</button>
+                        </div>
+                    </div>
+                    
                 </div>
         
-
+                
             </div>
 
 
